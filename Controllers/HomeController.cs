f402ddb0e7fe0f09c4de27a8622308e67318a15a
yawar2019 <yawarali17@gmail.com>
2022-10-22 22:05:58 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using CoreWebAppWithoutMVCFolder.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebAppWithoutMVCFolder.Controllers
+{
+    public class HomeController : Controller
+    {
+        IWebHostEnvironment _env;
+        IConfiguration _configuration;
+        public HomeController(IWebHostEnvironment env,IConfiguration configuration)
+        {
+            _env = env;
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            //var enviromentName = _env.EnvironmentName;
+            //Console.WriteLine(enviromentName);
+            //if (enviromentName == "Development")
+            //{
+            //    Console.WriteLine(enviromentName);
+            //    return Content(enviromentName);
+            //}
+
+
+            // var AdminName = _configuration.GetValue<string>("AdminName");
+            //var connectionstring = _configuration.GetConnectionString("sqlCon");
+            //var readEmployee = "Manager is "+_configuration.GetSection("Employees")["Manager"];
+            // var countOfEmployees = _configuration.GetValue<int>("Number_Of_Employees");  
+            var readEmployee = "Manager is " + _configuration.GetSection("Employees");
+            return Content(readEmployee[0].ToString());
+        }
+
+        public IActionResult GetAllEmployees()
+        {
+            EmployeeModel emp = new EmployeeModel();
+            emp.EmpId = 1;
+            emp.EmpName = "Keerti";
+            emp.EmpSalary = 21000;
+
+            return View(emp);
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebAppWithoutMVCFolder.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            ViewData["EmpName"] = "Shiva";
+        }
+
+        
+        public IActionResult OnPostEditEmployee()
+        {
+
+            ViewData["EmpName"] = "Bharti";
+
+            return Page();
+        }
+
+        public IActionResult OnPostSaveEmployee()
+        {
+
+            ViewData["EmpName"] = "Minkal";
+
+            return Page();
+        }
+    }
+}
